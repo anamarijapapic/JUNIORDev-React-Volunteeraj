b82728922906b401c8648b077b95a4cf38e62a7b
Anamarija Papić <anamarija.papic@agilo.com>
2024-04-29 10:41:02 +0200

    Firebase init
    - Firestore Database, Storage, Hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+// import { db, storageBucket } from './config/firebase';
 import { Button } from 'flowbite-react';
 
 function App() {
+  // Firestore database
+  // console.log(db);
+  // Storage bucket
+  // console.log(storageBucket);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
